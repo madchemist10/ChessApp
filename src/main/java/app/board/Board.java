a3f8be8b16b3203d67856d421bbc479dee94048a
MadChemist <2012kemp1234@gmail.com>
2017-07-24 21:51:43 -0500

    starting work on board creation

--- src/main/java/app/board/Board.java
+++ src/main/java/app/board/Board.java
@@ -8,4 +8,27 @@ package app.board;
  * board determines if the move that is desired is a valid move.
  */
 public class Board {
+    private static final int NUM_ROWS = 8;
+    private static final int NUM_COLS = 8;
+    private Square[][] squares = new Square[NUM_ROWS][NUM_COLS];
+
+    public Board(){
+
+    }
+
+    private void createSquares(){
+        for(int i = 0; i < NUM_ROWS; i++){
+            for(int j = 0; j < NUM_COLS; j++){
+                squares[i][j] = new Square();
+            }
+        }
+    }
+
+    private void createPieces(){
+
+    }
+
+    private void placePieces(){
+
+    }
 }

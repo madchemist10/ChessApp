8aab517b37acf5d51aeef76e534a9c95ead4236e
rfaulkenberry <rfaulkenberry@camgian.com>
2017-07-26 17:13:57 -0500

    Added initialization for board and squares

--- src/main/java/app/board/Board.java
+++ src/main/java/app/board/Board.java
@@ -3,32 +3,99 @@ package app.board;
 /**
  * This class represents a chess board.
  * This board contains 64 squares and maintains
- * the location of where each piece is on the board.
+ * the location of where each piece is on the board, as
+ * well as information about the game state.
  * All moves must be made via the board and the
  * board determines if the move that is desired is a valid move.
  */
 public class Board {
-    private static final int NUM_ROWS = 8;
-    private static final int NUM_COLS = 8;
-    private Square[][] squares = new Square[NUM_ROWS][NUM_COLS];
 
-    public Board(){
+	
+	
+	/** This array contains every square of the board, indexed by x and y axes,
+	 *  where the origin (squares[0][0]) is the white queenside rook's square.
+	 */
+	private Square[7][7] squares;
+	
+	/** These flags are true if the respective castle move is allowed, and must
+	 *  be altered if any castle rights are lost in the course of a game.
+	 */
+	private Boolean whiteCastleKingside;
+	private Boolean whiteCastleQueenside;
+	private Boolean blackCastleKingside;
+	private Boolean blackCastleQueenside;
+	
+	/** These flags are true if the respective king is currently in check. 
+	 * (This is potentially unneeded, value could be checked on the fly)
+	 */
+	private whiteCheck;
+	private blackCheck;
+	
+	/** Move counter, incremented after either player makes a move */
+	private short moveCount;
+	
+	Board() {
+		// Create squares and assign pieces
+		for (int x = 0; x < 8; x++) {
+			for (int y = 0; y < 8; y++) {
+				squares[x][y] = initSquare(x, y);
+			}
+		}
+		
+		// All castling rights exist at game start
+		whiteCastleKingside = true;
+		whiteCastleQueenside = true;
+		blackCastleKingside = true;
+		blackCastleQueenside = true;
+		
+		// No moves made yet
+		moveCounter = 0;
+		
+	}
+	
+	/** Helper function called by constructor to create the desired square
+	 *  and give it a piece if it needs one for the initial game state.
+	 */ 
+	private Square initSquare(x, y) {
+		Boolean occupied;
+		Color color;
+		PieceType type;
 
-    }
+		// Set the color if we're on a back rank, otherwise we have enough info
+		// to just create the square and return
+		switch (y) {
+		case 0: // White piece rank
+			color = WHITE;
+			break;
+		case 1: // White pawn rank
+			return new Square(x, y, true, new Pawn(WHITE));
+		case 6: // Black pawn rank
+			return new Square(x, y, true, new Pawn(BLACK));
+		case 7: // Black piece rank
+			color = BLACK;
+			break;
+		default:
+			return new Square(x, y, false, NULL);	
+		}
+		
+		// Back rank case, we need to determine what kind of piece this is
+		switch (x) {
+		case 0: // Rooks
+		case 7:
+			return new Square(x, y, true, new Rook(color));
+		case 1: // Knights
+		case 6:
+			return new Square(x, y, true, new Knight(color));
+		case 2: // Bishops
+		case 5:
+			return new Square(x, y, true, new Bishop(color));
+		case 3: // Queen
+			return new Square(x, y, true, new Queen(color));
+		case 4: // King
+			return new Square(x, y, true, new King(color));
+		}
 
-    private void createSquares(){
-        for(int i = 0; i < NUM_ROWS; i++){
-            for(int j = 0; j < NUM_COLS; j++){
-                squares[i][j] = new Square();
-            }
-        }
-    }
-
-    private void createPieces(){
-
-    }
-
-    private void placePieces(){
-
-    }
+		// Error case
+		return new Square(x, y, false, NULL);
+	}
 }
--- src/main/java/app/board/Square.java
+++ src/main/java/app/board/Square.java
@@ -8,4 +8,38 @@ package app.board;
  * another piece may take its location.
  */
 public class Square {
+
+	/**
+	 * x and y are the coordinates of the square on the board, where each
+	 * coordinate is in the range [0-7] and the origin square (x=0,y=0) is
+	 * the white queenside rook's square
+	 */
+	int x;
+	int y;
+	
+	/** Indicates whether the square is occupied */
+	Boolean occupied;
+	
+	/** The piece occupying this square. Undefined if not occupied. */
+	private Piece currentPiece;
+
+	Square(int x, int y, occupied, Piece) {
+		this.x = x;
+		this.y = y;
+		this.piece = piece;
+	}
+	
+	public Boolean isOccupied() {
+		if (occupied) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Piece getPiece() {
+		if (occupied) {
+			return currentPiece;
+		}
+		return null;
+	}
 }

06a2a808600c64ff3cf7ef38b43da9c2b007f1ed
MadChemist <2012kemp1234@gmail.com>
2017-07-29 21:33:18 -0500

    added king move set validation test implementation

--- src/test/java/app/PieceMoveValidationTests.java
+++ src/test/java/app/PieceMoveValidationTests.java
@@ -1,6 +1,8 @@
 package app;
 
 import app.piece.Bishop;
+import app.piece.IPiece;
+import app.piece.King;
 import app.piece.Moves;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,7 +23,7 @@ public class PieceMoveValidationTests {
      */
     @Test
     public void bishopMoveSetValidation(){
-        Bishop bishop = new Bishop(Color.WHITE);
+        IPiece bishop = new Bishop(Color.WHITE);
         List<Moves> moveSet = bishop.getMoveSet();
         List<Moves> BISHOP = new LinkedList<>();
         BISHOP.add(Moves.DIAGONAL_FORWARD_LEFT);
@@ -34,9 +36,30 @@ public class PieceMoveValidationTests {
         }
     }
 
+    /**
+     * Validate that each and only each move
+     * that should be a member of the king's
+     * move set are contained within that set.
+     */
     @Test
     public void kingMoveSetValidation(){
-
+        IPiece king = new King(Color.WHITE);
+        List<Moves> moveSet = king.getMoveSet();
+        List<Moves> KING = new LinkedList<>();
+        KING.add(Moves.FORWARD);
+        KING.add(Moves.BACKWARD);
+        KING.add(Moves.LEFT);
+        KING.add(Moves.RIGHT);
+        KING.add(Moves.DIAGONAL_FORWARD_LEFT);
+        KING.add(Moves.DIAGONAL_FORWARD_RIGHT);
+        KING.add(Moves.DIAGONAL_BACKWARD_LEFT);
+        KING.add(Moves.DIAGONAL_BACKWARD_RIGHT);
+        KING.add(Moves.CASTLE_LEFT);
+        KING.add(Moves.CASTLE_RIGHT);
+        Assert.assertEquals(KING.size(), moveSet.size());
+        for(Moves move: KING){
+            Assert.assertTrue(moveSet.contains(move));
+        }
     }
 
     @Test

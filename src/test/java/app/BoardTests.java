9790f41ebae1b1b17ea92a73e7dea6c6b92dbf58
MadChemist <2012kemp1234@gmail.com>
2017-07-30 13:58:37 -0500

    added default board creation validation test

--- src/test/java/app/BoardTests.java
+++ src/test/java/app/BoardTests.java
@@ -1,8 +1,23 @@
 package app;
 
+import app.board.Board;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * These tests are examples of how the
  * board reacts to specific functionality.
  */
 public class BoardTests {
+
+    /**
+     * This test is used to validate that the
+     * creation of a default board results in
+     * a board being created.
+     */
+    @Test
+    public void validateDefaultBoardCreation(){
+        Board board = new Board();
+        Assert.assertNotNull(board);
+    }
 }

2fdaf26962ba5ffffaa9e78e2d5d1b59ab0251cf
MadChemist <2012kemp1234@gmail.com>
2017-07-29 21:32:07 -0500

    added bishop move set validation test implementation

--- src/test/java/app/PieceMoveValidationTests.java
+++ src/test/java/app/PieceMoveValidationTests.java
@@ -1,6 +1,12 @@
 package app;
 
+import app.piece.Bishop;
+import app.piece.Moves;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * This set of tests are used to validate that
@@ -8,9 +14,24 @@ import org.junit.Test;
  */
 public class PieceMoveValidationTests {
 
+    /**
+     * Validate that each and only each move
+     * that should be a member of the bishop's
+     * move set are contained within that set.
+     */
     @Test
     public void bishopMoveSetValidation(){
-
+        Bishop bishop = new Bishop(Color.WHITE);
+        List<Moves> moveSet = bishop.getMoveSet();
+        List<Moves> BISHOP = new LinkedList<>();
+        BISHOP.add(Moves.DIAGONAL_FORWARD_LEFT);
+        BISHOP.add(Moves.DIAGONAL_FORWARD_RIGHT);
+        BISHOP.add(Moves.DIAGONAL_BACKWARD_LEFT);
+        BISHOP.add(Moves.DIAGONAL_BACKWARD_RIGHT);
+        Assert.assertEquals(BISHOP.size(), moveSet.size());
+        for(Moves move: BISHOP){
+            Assert.assertTrue(moveSet.contains(move));
+        }
     }
 
     @Test

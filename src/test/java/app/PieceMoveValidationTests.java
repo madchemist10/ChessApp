33d525390802d0e47034a6fe6d929f192bd57b81
MadChemist <2012kemp1234@gmail.com>
2017-07-29 21:27:11 -0500

    create test framework for testing that each set of moves for the pieces are valid

---
+++ src/test/java/app/PieceMoveValidationTests.java
@@ -0,0 +1,40 @@
+package app;
+
+import org.junit.Test;
+
+/**
+ * This set of tests are used to validate that
+ * each piece's move sets are correct.
+ */
+public class PieceMoveValidationTests {
+
+    @Test
+    public void bishopMoveSetValidation(){
+
+    }
+
+    @Test
+    public void kingMoveSetValidation(){
+
+    }
+
+    @Test
+    public void knightMoveSetValidation(){
+
+    }
+
+    @Test
+    public void pawnMoveSetValidation(){
+
+    }
+
+    @Test
+    public void queenMoveSetValidation(){
+
+    }
+
+    @Test
+    public void rookMoveSetValidation(){
+
+    }
+}

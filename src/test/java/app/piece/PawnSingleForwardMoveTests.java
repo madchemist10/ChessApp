db412a685627faa9a9c2eadc4c27d28a867f21c9
MadChemist <2012kemp1234@gmail.com>
2017-07-30 10:28:58 -0500

    added helper routine to determine valid piece move added new set of pawnSingleForwardMoveTests

---
+++ src/test/java/app/piece/PawnSingleForwardMoveTests.java
@@ -0,0 +1,38 @@
+package app.piece;
+
+import app.board.Board;
+import app.board.Square;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * This set of tests are used to validate
+ * that all 8 pawns of both black and white can make
+ * a single forward move.
+ */
+public class PawnSingleForwardMoveTests {
+
+
+    /**
+     * This test validates that a pawn
+     * that resides on square {1,0} has
+     * a valid move within it's list to move to
+     * {2,0}, that is a single step forward.
+     */
+    @Test
+    public void moveValidatePawn_r1_c0_Forward_r2_c0(){
+        Board board = new Board();
+        int currRow = 1;
+        int currCol = 0;
+        int destRow = 2;
+        int destCol = 0;
+        Square dest = new Square(destRow, destCol);
+        Square curr = board.getSquare(currRow, currCol);
+        IPiece piece = curr.getPiece();
+        Assert.assertTrue(piece instanceof Pawn);
+        ValidMove validMove = new ValidMove(Moves.FORWARD, 1);
+        PieceTests.validatePieceMove(piece, validMove);
+        Square next = MoveValidator.determineNextSquare(curr,validMove,board);
+        Assert.assertTrue(dest.equals(next));
+    }
+}
--- src/test/java/app/piece/PieceTests.java
+++ src/test/java/app/piece/PieceTests.java
@@ -1,9 +1,6 @@
 package app.piece;
 
-import app.board.Board;
-import app.board.Square;
 import org.junit.Assert;
-import org.junit.Test;
 
 import java.util.List;
 
@@ -15,24 +12,14 @@ import java.util.List;
 public class PieceTests{
 
     /**
-     * This test validates that a pawn
-     * that resides on square {1,0} has
-     * a valid move within it's list to move to
-     * {2,0}, that is a single step forward.
+     * Helper routine to validate that the given
+     * piece can make the specified move.
+     * @param piece that wants to move
+     * @param validMove that should be contained within
+     *                  the piece's valid moves.
      */
-    @Test
-    public void moveValidatePawn_r1_c0_Forward_r2_c0(){
-        Board board = new Board();
-        int currRow = 1;
-        int currCol = 0;
-        int destRow = 2;
-        int destCol = 0;
-        Square dest = new Square(destRow, destCol);
-        Square curr = board.getSquare(currRow, currCol);
-        IPiece piece = curr.getPiece();
-        Assert.assertTrue(piece instanceof Pawn);
+    static void validatePieceMove(IPiece piece, ValidMove validMove){
         List<ValidMove> validMoves = piece.getValidMoves();
-        ValidMove validMove = new ValidMove(Moves.FORWARD, 1);
         boolean moveContained = false;
         for(ValidMove move: validMoves){
             if(validMove.equals(move)){
@@ -41,7 +28,5 @@ public class PieceTests{
             }
         }
         Assert.assertTrue(moveContained);
-        Square next = MoveValidator.determineNextSquare(curr,validMove,board);
-        Assert.assertTrue(dest.equals(next));
     }
 }

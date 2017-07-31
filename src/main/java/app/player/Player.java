05ddeaa2f940951188ad2fd051da828cf15c05ec
MadChemist <2012kemp1234@gmail.com>
2017-07-30 20:08:58 -0500

    implemented player framework

--- src/main/java/app/player/Player.java
+++ src/main/java/app/player/Player.java
@@ -1,9 +1,28 @@
 package app.player;
 
+import app.Color;
+import app.board.Square;
+import app.piece.IPiece;
+import app.piece.ValidMove;
+
+import java.util.List;
+
 /**
  * This interface presents all functionality
  * that is required to interact with the game board
  * and make moves.
  */
 public interface Player {
+
+    Color getColor();
+
+    void makeMove(IPiece piece, Square dest);
+
+    void capturePiece(IPiece piece, IPiece dest);
+
+    void setTurn();
+
+    void endTurn();
+
+    List<ValidMove> getValidMoves(IPiece piece);
 }
